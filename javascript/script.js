a52c80bcfe45dd87a317b085085c263699711b9c
Yi <ydong27@bcit.ca>
2021-04-01 07:32:52 -0700

    created loop which checks to see if any objects in array contain user input, if not exits and pushes user input to array

--- javascript/script.js
+++ javascript/script.js
@@ -18,3 +18,24 @@ const courseList = [item1, item2, item3];
 do {
     var user_numbers = window.prompt('Enter a 4-digit number: ');
 } while (isNaN(user_numbers) === true && user_numbers.length !== 4);
+
+for(let course of courseList)
+{
+    while ((Object.values(course))[0].includes(user_numbers))
+    {
+        let cor_code = (Object.values(course))[0]
+        let cor_name = (Object.values(course))[1]
+        console.log(`Yes I am taking the course: ${cor_code} - ${cor_name}`);
+        user_numbers = window.prompt('Enter a 4-digit number: ');
+        if (!(Object.values(course))[0].includes(user_numbers)) {
+          break;
+        }
+    }
+}
+
+var new_code = {
+    code: user_numbers,
+    name: null
+};
+courseList.push(new_code);
+console.log('New course code entered and saved.');
